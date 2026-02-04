b0212c45f0dbbef4f2f486dcbc62d5910e32b198
asgn200866 <homemail200866@gmail.com>
2026-02-04 19:21:06 +0300

    Типы данных. Создание простого калькулятора сложения

--- main_folder/script.js
+++ main_folder/script.js
@@ -4,3 +4,14 @@ alert("Hello");
 let name = prompt("Как вас зовут?");
 let isAdult = confirm("Вам есть 18?");
 */
+/*
+let sum1 = +prompt("Введите первое число");
+console.log(sum1);
+console.log(typeof sum1);
+let sum2 = +prompt("Введите второе число");
+console.log(sum2);
+console.log(typeof sum2);
+let summa = sum1 + sum2;
+console.log(summa);
+alert("Сумма двух чисел равна " + summa);
+*/
