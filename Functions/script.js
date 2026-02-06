bfab19f49e4965c4029bbd75b66e1a623e779f72
asgn200866 <homemail200866@gmail.com>
2026-02-06 23:53:10 +0300

    Функции JS

---
+++ Functions/script.js
@@ -0,0 +1,14 @@
+let a = +prompt("Введите первое число");
+let b = +prompt("Введите второе число");
+
+function summa(a, b) {
+  let sum = a + b;
+  console.log(sum);
+}
+
+function parity_a(a, b) {
+  if (a % 2 == 1) console.log("Число нечетное");
+  else console.log("Число нечетное");
+}
+
+summa(a, b);
