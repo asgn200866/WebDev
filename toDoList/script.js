829340c70dde66622374739ef043d47edf9bdfec
asgn200866 <homemail200866@gmail.com>
2026-02-15 19:38:31 +0300

    TodoList. JS.

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,5 +1,20 @@
-const labelTextTasks = document.querySelectorAll(".label-text-tasks");
+const labelTextTasks = document.getElementsByClassName("label-text-tasks");
+const elColumnTasks = document.getElementById("tasksText");
 
-labelTextTasks.forEach((string, index) => {
-  console.log(`string ${index}`, string.outerHTML);
+let fragmentTasksMain = document.createElement("div");
+fragmentTasksMain.className = "label-text-tasks";
+fragmentTasksMain.innerHTML = `<input type="checkbox" class="tasks-cb"><input type="text" class="tasks-input">`;
+elColumnTasks.insertAdjacentElement("beforeend", fragmentTasksMain);
+
+function TaskArr(text, index) {
+  this.text = text;
+  this.index = index;
+}
+
+Array.from(labelTextTasks).forEach((string, index) => {
+  console.log(`string ${index}`);
+  let fragmentTasksTest = string.outerHTML;
+  console.log(fragmentTasksTest);
 });
+
+/**inputArea.addEventListener("input", textTracker); */
