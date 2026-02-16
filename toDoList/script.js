1f1d2240b817dcb58826f744dcdbc589ed23d65b
asgn200866 <homemail200866@gmail.com>
2026-02-16 23:32:50 +0300

    TodoList. JS.

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,20 +1,23 @@
-const labelTextTasks = document.getElementsByClassName("label-text-tasks");
-const elColumnTasks = document.getElementById("tasksText");
-
-let fragmentTasksMain = document.createElement("div");
+const labelTextTasks = document.getElementsByClassName("tasks-input"); // HTMLcollection for tasks
+const elColumnTasks = document.getElementById("tasksText"); // DOMelement tasks
+const lastFragment = labelTextTasks[labelTextTasks - 1]; // Получение последнего элемента
+/*
+let fragmentTasksMain = document.createElement("div"); 
 fragmentTasksMain.className = "label-text-tasks";
 fragmentTasksMain.innerHTML = `<input type="checkbox" class="tasks-cb"><input type="text" class="tasks-input">`;
 elColumnTasks.insertAdjacentElement("beforeend", fragmentTasksMain);
+создание прототипа, не обьект
+*/
+console.log(lastFragment);
+console.log(labelTextTasks);
 
 function TaskArr(text, index) {
   this.text = text;
   this.index = index;
+} // Обьект tasks
+
+lastFragment.addEventListener("input", saveTextTasks);
+
+function saveTextTasks() {
+  var textTasksValue = lastFragment.textContent; // Получение текста из последнего обьекта
 }
-
-Array.from(labelTextTasks).forEach((string, index) => {
-  console.log(`string ${index}`);
-  let fragmentTasksTest = string.outerHTML;
-  console.log(fragmentTasksTest);
-});
-
-/**inputArea.addEventListener("input", textTracker); */
